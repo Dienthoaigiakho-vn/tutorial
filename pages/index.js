a71ca68380e2ac914c73f8c617db1501641741ad
luu <phuocluu1908@gmail.com>
2020-12-08 11:41:08 +0700

    new commit

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>My first next js project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          My first <a href="https://nextjs.org">Next.js</a> project
         </h1>
 
         <p className={styles.description}>
